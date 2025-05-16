c2e04377ea1ec0425949b55b2fdd1c3981750710
anshuu23 <anshumj321@gmail.com>
2025-05-16 22:11:40 +0530

    fix : removed unnecessary code

--- src/components/uploads/index.tsx
+++ src/components/uploads/index.tsx
@@ -103,26 +103,7 @@ const Uploads: React.FC = () => {
     return (
         <>{
             signedUrl &&
-            // <Dragger {...props}>
-
-            //     {
-            //         !isFileDroped &&
-            //         <>
-
-            //             <p className="ant-upload-drag-icon">
-            //                 <InboxOutlined />
-            //             </p>
-            //             <p className="ant-upload-text">Click or drag file to this area to upload</p>
-            //             <p className="ant-upload-hint">
-            //                 Support for a single or bulk upload. Strictly prohibited from uploading company data or other
-            //                 banned files.
-            //             </p>
-            //         </>
-            //     }
-
-            // </Dragger>
-
-
+            
              <Upload {...props}>
                 <Button icon={<UploadOutlined />}>Upload</Button>
             </Upload>
